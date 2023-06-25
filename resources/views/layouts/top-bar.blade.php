6094c4670b7a2c076242b1e7b1526c7690de73b1
anurag-viitorcloud <anurag.trivedi@viitor.cloud>
2023-06-25 08:15:35 +0530

    Changed file name

--- resources/views/layouts/top-bar.blade.php
+++ resources/views/layouts/top-bar.blade.php
@@ -35,7 +35,7 @@
               <hr class="dropdown-divider">
             </li>
 
-            <li>
+            {{-- <li>
               <a class="dropdown-item d-flex align-items-center" href="users-profile.html">
                 <i class="bi bi-person"></i>
                 <span>My Profile</span>
@@ -63,7 +63,7 @@
             </li>
             <li>
               <hr class="dropdown-divider">
-            </li>
+            </li> --}}
 
             <li>
               <form action="/logout" method="POST">
